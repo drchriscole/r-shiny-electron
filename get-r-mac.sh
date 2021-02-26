9f093deb5d889cb65be59833c0a22ffe561502e1
Chris Cole <c.cole@dundee.ac.uk>
2021-02-26 18:22:29 +0000

    updating url to mac pkg and removing faulty R script patches

--- get-r-mac.sh
+++ get-r-mac.sh
@@ -5,7 +5,7 @@ set -e
 # Requires xar and cpio, both installed in the Dockerfile
 mkdir -p r-mac
 curl -o r-mac/latest_r.pkg \
-     https://cloud.r-project.org/bin/macosx/R-3.5.1.pkg
+     https://mac.r-project.org/el-capitan/R-3.6-branch/R-3.6-branch-el-capitan.pkg    
 
 cd r-mac
 xar -xf latest_r.pkg
@@ -15,11 +15,11 @@ mv R.framework/Versions/Current/Resources/* .
 rm -r r.pkg R.framework
 
 # Patch the main R script
-sed -i.bak '/^R_HOME_DIR=/d' bin/R
-sed -i.bak 's;/Library/Frameworks/R.framework/Resources;${R_HOME};g' \
-    bin/R
-chmod +x bin/R
-rm -f bin/R.bak
+#sed -i.bak '/^R_HOME_DIR=/d' bin/R
+#sed -i.bak 's;/Library/Frameworks/R.framework/Resources;${R_HOME};g' \
+#    bin/R
+#chmod +x bin/R
+#rm -f bin/R.bak
 
 # Remove unneccessary files TODO: What else
 rm -r doc tests
